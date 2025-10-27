15917fb227f99dbb50b4a70c817ede5f40ff926a
mshyndaa <mshyndamaail@gmail.com>
2025-10-27 09:07:11 +0700

    20251027_0907

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -98,7 +98,7 @@
                             'personnels',
                             'createPersonnel',
                             'editPersonnel',
-                            'showpersonnels',
+                            // 'showpersonnels/{hash}',
                             'jobapplicant',
                             'changestos',
                             'showApplicant',
@@ -112,7 +112,7 @@
                             'personnels',
                             'createPersonnel',
                             'editPersonnel',
-                            'showpersonnels',
+                            // 'showpersonnels/{hash}',
                             'jobapplicant',
                             'changestos',
                             'showApplicant',
@@ -129,7 +129,7 @@
                                 'personnels',
                                 'createPersonnel',
                                 'editPersonnel',
-                                'showpersonnels',
+                                // 'showpersonnels/{hash}',
                                 'jobapplicant',
                                 'showApplicant',
                                 'showcareers',
@@ -145,7 +145,7 @@
                                             'personnels',
                                             'createPersonnel',
                                             'editPersonnel',
-                                            'showpersonnels',
+                                            // 'showpersonnels/{hash}',
                                             'jobapplicant',
                                             'changestos',
                                             'editChangesto',
@@ -177,7 +177,7 @@
                                     'personnels',
                                     'createPersonnel',
                                     'editPersonnel',
-                                    'showpersonnels',
+                                    // 'showpersonnels/{hash}',
                                     'jobapplicant',
                                     'showcareers',
                                     'changestos',
@@ -188,14 +188,14 @@
                                 ])) {{ 'hidden' }} @endif mt-1 pl-8"
                                 :class="open ? 'block!' : 'hidden'">
                                 <li class="mb-1 last:mb-0">
-                                    <a class="@if (Route::is('personnels', 'createPersonnel', 'editPersonnel', 'showpersonnels')) {{ 'text-violet-500!' }} @endif block truncate text-gray-500/90 transition hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                                    <a class="@if (Route::is('personnels', 'createPersonnel', 'editPersonnel', 'showpersonnels/{hash}')) {{ 'text-violet-500!' }} @endif block truncate text-gray-500/90 transition hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                                         href="{{ route('personnels') }}">
                                         <span
                                             class="lg:sidebar-expanded:opacity-100 text-m font-medium duration-200 lg:opacity-0 2xl:opacity-100">PRF</span>
                                     </a>
                                 </li>
                                 <li class="mb-1 last:mb-0">
-                                    <a class="@if (Route::is('jobapplicant', 'showcareers')) {{ 'text-violet-500!' }} @endif block truncate text-gray-500/90 transition hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
+                                    <a class="@if (Route::is('jobapplicant')) {{ 'text-violet-500!' }} @endif block truncate text-gray-500/90 transition hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200"
                                         href="{{ route('jobapplicant') }}">
                                         <span
                                             class="lg:sidebar-expanded:opacity-100 text-m font-medium duration-200 lg:opacity-0 2xl:opacity-100">Applicant
@@ -232,7 +232,7 @@
                                             stroke-width="1.5" stroke="currentColor" width="16" height="16">
                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                 d=" M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342
-                                                                                                                                                                                                        1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                                                                                                                                                                                                                                                    1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
                                         </svg>
                                         <span
                                             class="lg:sidebar-expanded:opacity-100 text-m ml-4 font-medium duration-200 lg:opacity-0 2xl:opacity-100">Budget</span>
