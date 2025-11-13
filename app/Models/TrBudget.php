5e963a42508f20f38da09adaab5048285d07a5e3
weha87 <75598452+weha87@users.noreply.github.com>
2025-11-13 16:52:27 +0700

    251113 Williem Update

--- app/Models/TrBudget.php
+++ app/Models/TrBudget.php
@@ -8,14 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class TrBudget extends Model
 {
     use HasFactory;
-
-
     protected $connection = 'pgsql';
     protected $table = 'tr_budget';   
     protected $fillable = [     
-        'budget_id', 'budget_date', 'perpost', 'cpny_id', 'business_unit_id', 'department_fin_id', 'totalbudget', 
-        'status', 'created_by', 'created_at', 'updated_by', 'updated_at', 'deleted_by', 'deleted_at', 'completed_by',
-        'completed_at'
+        'refnbr' , 'prev_refnbr' , 'doctype' , 'submitdate' , 'perpost_year' , 'perpost_month' , 'cpny_id' , 'business_unit_id' , 
+        'department_fin_id' , 'account_id' , 'activity_id' , 'activity_descr' , 'activity_type' , 'budget_type' , 
+        'trancation_activity' , 'budget_amount' , 'status' , 'created_by' , 'created_at' , 
+        'updated_by' , 'updated_at' , 'deleted_by' , 'deleted_at'
     ];
-   
+
 }
