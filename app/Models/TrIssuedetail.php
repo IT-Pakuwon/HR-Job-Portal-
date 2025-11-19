21995ba0c2629dc07f82544708b54e4a4ed42a63
weha87 <75598452+weha87@users.noreply.github.com>
2025-11-19 10:08:34 +0700

    251119 Williem Update Model

--- app/Models/TrIssuedetail.php
+++ app/Models/TrIssuedetail.php
@@ -42,10 +42,10 @@ class TrIssuedetail extends Model
         // 'created_by',
         // 'updated_by'
         'issueid' , 'issue_no' , 'spbid' , 'spb_no' , 'issuetype' , 'inventoryid' , 'inventory_descr' , 'siteid' , 'qty' , 'uom' , 
-        'type_multiplier' , 'base_multiplier' , 'base_qty' , 'base_uom' , 'unitcost' , 'totalcost' , 'note' , 'location_id' , 
-        'sub_location_id' , 'budget_perpost' , 'budget_cpny_id' , 'budget_business_unit_id' , 'budget_department_fin_id' , 
-        'budget_account_id' , 'budget_activity_id' , 'reason_code' , 'issue_qty' , 'status' , 'created_by' , 'created_at' , 
-        'updated_by' , 'updated_at' , 'deleted_by' , 'deleted_at' , 'completed_by' , 'completed_at'
+        'type_multiplier' , 'base_multiplier' , 'base_qty' , 'base_uom' , 'unitcost' , 'totalcost' , 'issuenote_detail' , 
+        'location_id',         'sub_location_id' , 'budget_perpost' , 'budget_cpny_id' , 'budget_business_unit_id' , 
+        'budget_department_fin_id', 'budget_account_id' , 'budget_activity_id' , 'reason_code' , 'issue_qty' , 'status' , 
+        'created_by' , 'created_at' , 'updated_by' , 'updated_at' , 'deleted_by' , 'deleted_at' , 'completed_by' , 'completed_at'
     ];
 
     // location_id (FK) -> MsLocationPG.locationid (PK)
--- app/Models/TrSPPBdetail.php
+++ app/Models/TrSPPBdetail.php
@@ -11,12 +11,12 @@ class TrSPPBdetail extends Model
     protected $table = "tr_sppb_detail";
 
     protected $fillable = [
-        'sppbid' , 'sppb_no' , 'inventory_type' , 'inventory_sub_type' , 'inventory_category' , 'inventoryid' , 'inventory_descr' , 'qty' , 'uom' , 
-        'siteid' , 'type_multiplier' , 'base_multiplier' , 'base_qty' , 'base_uom' , 'note' , 'location_id' , 'sub_location_id' , 
-        'budget_perpost' , 'budget_cpny_id' , 'budget_business_unit_id' , 'budget_department_fin_id' , 'budget_account_id' , 
-        'budget_activity_id' , 'budget_activity_descr' , 'assignby' , 'assigndate' , 'assignpurchasing' , 'openordered' , 
-        'ordered' , 'rejectordered' , 'completeordered' , 'status' , 'created_by' , 'created_at' , 'updated_by' , 'updated_at' , 
-        'deleted_by' , 'deleted_at' , 'completed_by' , 'completed_at'
+        'sppbid' , 'sppb_no' , 'inventory_type' , 'inventory_sub_type' , 'inventory_category' , 'inventoryid' , 'inventory_descr' , 
+        'qty' , 'uom' , 'siteid' , 'type_multiplier' , 'base_multiplier' , 'base_qty' , 'base_uom' , 'note' , 'location_id' , 
+        'sub_location_id' , 'budget_perpost' , 'budget_cpny_id' , 'budget_business_unit_id' , 'budget_department_fin_id' , 
+        'budget_account_id' , 'budget_activity_id' , 'budget_activity_descr' , 'assignby' , 'assigndate' , 'assignpurchasing' , 
+        'openordered' , 'ordered' , 'rejectordered' , 'completeordered' , 'spbid' , 'spb_no' , 'status' , 'created_by' , 'created_at' , 
+        'updated_by' , 'updated_at' , 'deleted_by' , 'deleted_at' , 'completed_by' , 'completed_at'
     ];
 
     // location_id (FK) -> MsLocationPG.locationid (PK)
