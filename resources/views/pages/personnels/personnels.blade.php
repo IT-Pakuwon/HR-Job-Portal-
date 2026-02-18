a4f5b2112e3b247241e59e17d6d023cc05dba688
Bedria Mashyanda Maail <119824438+mshyndaa@users.noreply.github.com>
2026-02-18 14:31:20 +0700

    20260218_1431

--- resources/views/pages/personnels/personnels.blade.php
+++ resources/views/pages/personnels/personnels.blade.php
@@ -135,162 +135,161 @@
         $(document).ready(function() {
             // Hanya inisialisasi tabel personnelsTable
             let personnelsTable = $('#personnelsTable').DataTable({
-                ajax: "{{ route('personnels.json') }}?status=P",
-                processing: true,
-                serverSide: false,
-                lengthMenu: [
-                    [10, 25, 50, 100, 250, -1],
-                    [10, 25, 50, 100, 250, 'All']
-                ],
-                responsive: {
-                    details: {
-                        type: 'column',
-                        target: 0 // 👈 this is REQUIRED
-                    }
-                },
-
-                columnDefs: [{
-                    targets: 0,
-                    width: '28px',
-                    className: 'dtr-control',
-                    orderable: false
-                }],
-                dom: '<"dt-toolbar flex items-center justify-start gap-4"lBf>rtip',
-                buttons: [{
-                        extend: 'excelHtml5',
-                        text: '↓ Excel',
-                        title: 'list_PO',
-                        className: 'bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700',
-                        exportOptions: {
-                            columns: ':visible',
-                            modifier: {
-                                page: 'current'
-                            }
+                    ajax: "{{ route('personnels.json') }}?status=P",
+                    processing: true,
+                    serverSide: false,
+                    lengthMenu: [
+                        [10, 25, 50, 100, 250, -1],
+                        [10, 25, 50, 100, 250, 'All']
+                    ],
+                    responsive: {
+                        details: {
+                            type: 'column',
+                            target: 0 // 👈 this is REQUIRED
                         }
                     },
-                    {
-                        extend: 'csvHtml5',
-                        text: '↓ CSV',
-                        title: 'list_PO',
-                        className: 'bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700',
-                        exportOptions: {
-                            columns: ':visible',
-                            modifier: {
-                                page: 'current'
+
+                    columnDefs: [{
+                        targets: 0,
+                        width: '28px',
+                        className: 'dtr-control',
+                        orderable: false
+                    }],
+                    dom: '<"dt-toolbar flex items-center justify-start gap-4"lBf>rtip',
+                    buttons: [{
+                            extend: 'excelHtml5',
+                            text: '↓ Excel',
+                            title: 'list_PO',
+                            className: 'bg-green-600 text-white px-3 py-1 rounded hover:bg-green-700',
+                            exportOptions: {
+                                columns: ':visible',
+                                modifier: {
+                                    page: 'current'
+                                }
+                            }
+                        },
+                        {
+                            extend: 'csvHtml5',
+                            text: '↓ CSV',
+                            title: 'list_PO',
+                            className: 'bg-blue-600 text-white px-3 py-1 rounded hover:bg-blue-700',
+                            exportOptions: {
+                                columns: ':visible',
+                                modifier: {
+                                    page: 'current'
+                                }
                             }
                         }
-                    }
-                ],
+                    ],
 
 
-                // order: [1, 'asc'],
-                ordering: false,
+                    // order: [1, 'asc'],
+                    ordering: false,
 
-                rowGroup: {
-                    dataSrc: 'cpnyid', // Kelompokkan berdasarkan kolom 'cpnyid'
-                    startRender: function(rows, group) {
-                        // Cek apakah semua baris dalam grup saat ini tersembunyi (collapsed)
-                        let isCollapsed = rows.nodes().to$().filter('.collapsed-group-row').length ===
-                            rows.count();
-                        let icon = isCollapsed ? '<i class="fas fa-plus-circle"></i>' :
-                            '<i class="fas fa-minus-circle"></i>';
+                    rowGroup: {
+                        dataSrc: 'cpnyid', // Kelompokkan berdasarkan kolom 'cpnyid'
+                        startRender: function(rows, group) {
+                            // Cek apakah semua baris dalam grup saat ini tersembunyi (collapsed)
+                            let isCollapsed = rows.nodes().to$().filter('.collapsed-group-row').length ===
+                                rows.count();
+                            let icon = isCollapsed ? '<i class="fas fa-plus-circle"></i>' :
+                                '<i class="fas fa-minus-circle"></i>';
 
-                        // Mengembalikan baris grup dengan ikon dan jumlah catatan
-                        return $('<tr/>')
-                            .append('<td colspan="' + rows.columns().count() + '">' + icon + ' ' +
-                                group + ' (' + rows.count() + ' records)</td>')
-                            .attr('data-group', group)
-                            .addClass('group-row');
-                    }
-                },
-                columns: [{
-                        data: null,
-                        defaultContent: ''
-                    }, // DTR control
-                    {
-
-                        data: 'eid',
-                        render: function(data, type, row) {
-                            let url = `/showpersonnels/${row.eid}`;
-                            let buttonClass =
-                                'inline-flex justify-center items-center w-[120px] px-3 py-1.5 text-sm leading-tight font-semibold text-white rounded text-center transition-colors duration-200  bg-gray-600 hover:bg-gray-700 ';
-                            let buttonText = row.docid; // Menggunakan row.docid untuk teks tombol
-
-                            // Cek apakah user yang login sama dengan created_user dan status = D (Revise/Draft)
-                            if (row.status === 'D' && row.created_user === currentUser) {
-                                url = `/editpersonnels/${row.eid}`;
-                                buttonClass =
-                                    'inline-flex justify-center items-center w-[120px] px-3 py-1.5 text-sm leading-tight font-semibold text-white rounded text-center transition-colors duration-200 bg-yellow-500 hover:bg-yellow-700';
-                            }
-
-                            return `<a href="${url}" class="px-3 py-1 ${buttonClass} text-white rounded">${buttonText}</a>`;
+                            // Mengembalikan baris grup dengan ikon dan jumlah catatan
+                            return $('<tr/>')
+                                .append('<td colspan="' + rows.columns().count() + '">' + icon + ' ' +
+                                    group + ' (' + rows.count() + ' records)</td>')
+                                .attr('data-group', group)
+                                .addClass('group-row');
                         }
                     },
-                    {
-                        data: 'date',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'cpnyid',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'division_id',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'departementid',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'job_title',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'job_level',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'created_user',
-                        className: 'no-pointer'
-                    },
-                    {
-                        data: 'status',
-                        className: 'no-pointer',
-                        render: function(data) {
-                            let statusText = "";
-                            let badgeClass = "";
+                    columns: [{
+                            data: null,
+                            defaultContent: ''
+                        }, // DTR control
+                        {
 
-                            if (data === 'D') {
-                                statusText = "Revise";
-                                badgeClass =
-                                    "w-32 bg-amber-200/60 text-amber-800 dark:bg-amber-300/40 dark:text-amber-900 focus:outline-none pointer-events-none border border-amber-600/40 font-semibold px-4 py-2 text-center rounded";
-                            } else if (data === 'P') {
-                                statusText = "On Progress";
-                                badgeClass =
-                                    "w-32 bg-orange-200/60 text-orange-800 dark:bg-orange-300/40 dark:text-orange-900 focus:outline-none pointer-events-none border border-orange-600/40 font-semibold px-4 py-2 text-center rounded";
+                            data: 'eid',
+                            render: function(data, type, row) {
+                                let url = `/showpersonnels/${row.eid}`;
+                                let buttonClass =
+                                    'inline-flex justify-center items-center w-[120px] px-3 py-1.5 text-sm leading-tight font-semibold text-white rounded text-center transition-colors duration-200  bg-gray-600 hover:bg-gray-700 ';
+                                let buttonText = row.docid; // Menggunakan row.docid untuk teks tombol
+
+                                // Cek apakah user yang login sama dengan created_user dan status = D (Revise/Draft)
+                                if (row.status === 'D' && row.created_user === currentUser) {
+                                    url = `/editpersonnels/${row.eid}`;
+                                    buttonClass =
+                                        'inline-flex justify-center items-center w-[120px] px-3 py-1.5 text-sm leading-tight font-semibold text-white rounded text-center transition-colors duration-200 bg-yellow-500 hover:bg-yellow-700';
+                                }
+
+                                return `<a href="${url}" class="px-3 py-1 ${buttonClass} text-white rounded">${buttonText}</a>`;
                             }
-                        } else if (data === 'C') {
-                            statusText = "Completed";
-                            badgeClass =
-                                "w-32 bg-green-200/60 text-green-800 dark:bg-green-300/40 dark:text-green-900 focus:outline-none pointer-events-none border border-green-600/40 font-semibold px-4 py-2 text-center rounded";
-                        } else if (data === 'X') {
-                            statusText = "Cancel";
-                            badgeClass =
-                                "w-32 bg-red-200/60 text-red-800 dark:bg-red-300/40 dark:text-red-900 focus:outline-none pointer-events-none border border-red-600/40 font-semibold px-4 py-2 text-center rounded";
-                        } else if (data === 'R') {
-                            statusText = "Rejected";
-                            badgeClass =
-                                "w-32 bg-red-200/60 text-red-800 dark:bg-red-300/40 dark:text-red-900 focus:outline-none pointer-events-none border border-red-600/40 font-semibold px-4 py-2 text-center rounded";
-                        } else {
-                            badgeClass =
-                                "w-32 bg-gray-200/60 text-gray-700 dark:bg-gray-300/40 dark:text-gray-900 focus:outline-none pointer-events-none border border-gray-500/40 font-semibold px-4 py-2 text-center rounded";
+                        },
+                        {
+                            data: 'date',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'cpnyid',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'division_id',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'departementid',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'job_title',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'job_level',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'created_user',
+                            className: 'no-pointer'
+                        },
+                        {
+                            data: 'status',
+                            className: 'no-pointer',
+                            render: function(data) {
+                                let statusText = "";
+                                let badgeClass = "";
+
+                                if (data === 'D') {
+                                    statusText = "Revise";
+                                    badgeClass =
+                                        "w-32 bg-amber-200/60 text-amber-800 dark:bg-amber-300/40 dark:text-amber-900 focus:outline-none pointer-events-none border border-amber-600/40 font-semibold px-4 py-2 text-center rounded";
+                                } else if (data === 'P') {
+                                    statusText = "On Progress";
+                                    badgeClass =
+                                        "w-32 bg-orange-200/60 text-orange-800 dark:bg-orange-300/40 dark:text-orange-900 focus:outline-none pointer-events-none border border-orange-600/40 font-semibold px-4 py-2 text-center rounded";
+                                } else if (data === 'C') {
+                                    statusText = "Completed";
+                                    badgeClass =
+                                        "w-32 bg-green-200/60 text-green-800 dark:bg-green-300/40 dark:text-green-900 focus:outline-none pointer-events-none border border-green-600/40 font-semibold px-4 py-2 text-center rounded";
+                                } else if (data === 'X') {
+                                    statusText = "Cancel";
+                                    badgeClass =
+                                        "w-32 bg-red-200/60 text-red-800 dark:bg-red-300/40 dark:text-red-900 focus:outline-none pointer-events-none border border-red-600/40 font-semibold px-4 py-2 text-center rounded";
+                                } else if (data === 'R') {
+                                    statusText = "Rejected";
+                                    badgeClass =
+                                        "w-32 bg-red-200/60 text-red-800 dark:bg-red-300/40 dark:text-red-900 focus:outline-none pointer-events-none border border-red-600/40 font-semibold px-4 py-2 text-center rounded";
+                                } else {
+                                    badgeClass =
+                                        "w-32 bg-gray-200/60 text-gray-700 dark:bg-gray-300/40 dark:text-gray-900 focus:outline-none pointer-events-none border border-gray-500/40 font-semibold px-4 py-2 text-center rounded";
+                                }
+                            }
+                            return `<span class="${badgeClass}">${statusText}</span>`;
                         }
-                    }
-                    return `<span class="${badgeClass}">${statusText}</span>`;
-                }
-            }]
-        });
+                    }]
+            });
 
         // Event listener untuk klik pada baris grup (collapse/expand) untuk personnelsTable
         $('#personnelsTable tbody').on('click', 'tr.group-row', function() {
@@ -316,12 +315,12 @@
 
         // Filter status akan memfilter data di personnelsTable
         $('.status-filter').on('click', function(e) {
-        e.preventDefault();
-        let selectedStatus = $(this).data('status');
-        let newUrl = "{{ route('personnels.json') }}";
-        newUrl += "?status=" + encodeURIComponent(selectedStatus ?? '');
-        console.log("Loading personnelsTable with URL:", newUrl);
-        personnelsTable.ajax.url(newUrl).load();
+            e.preventDefault();
+            let selectedStatus = $(this).data('status');
+            let newUrl = "{{ route('personnels.json') }}";
+            newUrl += "?status=" + encodeURIComponent(selectedStatus ?? '');
+            console.log("Loading personnelsTable with URL:", newUrl);
+            personnelsTable.ajax.url(newUrl).load();
         });
         });
         // Make each .grid-col-1 set independent
